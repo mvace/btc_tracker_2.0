33aad99d6ed784c9ce470d2e47f304f28e4386e7
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-08-24 07:32:56 +0200

    Start implementing auth system: password hashing and JWT basics

--- services/portfolio_service/app/main.py
+++ services/portfolio_service/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routers import portfolios, transactions
 from core.settings import settings
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 
 app = FastAPI(title="Portfolio Microservice")
@@ -12,3 +16,11 @@ app.include_router(transactions.router, prefix="/transactions", tags=["Transacti
 @app.get("/health", tags=["meta"])
 async def health():
     return {"status": "ok", "env": settings.APP_ENV}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
