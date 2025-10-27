885afe73ef920468a69ec4a7f356c16baa3a97df
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-10-27 17:40:38 +0100

    New demo account set in the login view for viewing the app without creating an account

--- services/ui/views/login_view.py
+++ services/ui/views/login_view.py
@@ -6,7 +6,7 @@ from components.forms import login_form
 def login_view():
     st.write("Or, try the app with a demo account:")
     if st.button("🚀 Use Demo Account"):
-        if auth.login_user("bob@example.com", "bobpass"):
+        if auth.login_user("demoaccount@example.com", "demopass"):
             st.rerun()
 
     submitted, username, password = login_form()
